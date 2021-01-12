ed5daa33cd1c7e5e343583f6ca9f4ec3b79a2a12
Stacey McKnight <mcknight.stm@gmail.com>
2021-01-12 10:38:12 -0600

    commit tree 5dd87da70c94f80a36cec51fb6cfaa71f6177391

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 18, "M", "Brown", "nice", 1),
+(2, "Rowdy", 16, "M", "Black", "nice", 1),
+(3, "Tabitha", 20, "F", "Brown", "nice", 1),
+(4, "Sergeant Brown", 2, "M", "Brown", "nice", 1),
+(5, "Melissa", 4, "F", "Black", "nice", 1),
+(6, "Grinch", 19, "M", "Brown", "nice", 1),
+(7, "Wendy", 1, "F", "Brown", "nice", 1),
+(8, NULL, 10, "M", "Brown", "nice", 1);
